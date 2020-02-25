c287d0a0ac38b4d5c6ed6ac0e0f62ac605b5a5ca
external.martin.patsov <external.martin.patsov@kuehne-nagel.com>
2020-02-26 00:56:57 +0200

    Create my first PR

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo 'Hello fmi. Get ready to hack.';
+echo '<p>Hello fmi. Get ready to hack.</p>';
+
+echo '<p>Pssst.. any fmi ideas go <a href="https://fss.fmi.uni-sofia.bg/za-nas/kutiq-za-idei/" target="_blank">here</a></p>';
 
 ?>
