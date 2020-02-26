5df5921cdd56c4544c47ade6d4cd58752a4c8eb5
unknown <andonovx@outlook.com>
2020-02-26 19:07:55 +0200

    Task done according to the task description.

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+echo "<table>";
+for ($i=1;$i<10;$i++)
+{
+	echo "<tr>";
+	for ($j=1;$j<10;$j++)
+	{
+		if ($i==1)
+			echo "<th> $j </th>";
+		else if ($j==1)
+		{
+			echo "<th> $i </th>";
+		}
+		else 
+			echo "<td>". $i*$j ."</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
 
 echo '<p>Hello fmi. Get ready to hack.</p>';
 
