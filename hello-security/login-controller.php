c903a293b5fd5f1a7c766d2ed74148f4f729d1f8
external.martin.patsov <external.martin.patsov@kuehne-nagel.com>
2020-04-14 22:50:58 +0300

    Add url

--- hello-security/login-controller.php
+++ hello-security/login-controller.php
@@ -1,16 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+<a href="index.php"> Take me home, country roads ... </a> <br>
 <?php
 include "db-connection.php";
 $conn = openCon();
 
-$sql = "SELECT * from person where email = '" . htmlentities($_POST["email"]) . "' and password = '" . htmlentities($_POST["password"]) ."';" ;
+$sql = "SELECT * from person where email = '" . htmlentities($_POST["email"]) . "' and password = '" . htmlentities($_POST["password"]) . "';";
 
-$resultSet = $conn -> query($sql) or die("Failed to query from DB!");
+$resultSet = $conn->query($sql) or die("Failed to query from DB!");
 
 $firstrow = $resultSet->fetch(PDO::FETCH_ASSOC) or die ("Not valid credentials.");
 
-echo ("Hello " . $firstrow['firstname']. " you are now logged in.");
+echo("Hello " . $firstrow['firstname'] . " you are now logged in.");
 
 session_start();
 $_SESSION["email"] = $firstrow['email'];
 
 ?>
+</body>
+</html>
